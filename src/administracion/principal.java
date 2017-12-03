c196520dce82ba17168ab325cc23b0b9a185af46
Sonivox <31835570+Sonivox@users.noreply.github.com>
2017-12-03 14:53:23 -0700

    ahora si

---
+++ src/administracion/principal.java
@@ -0,0 +1,16 @@
+package administracion;
+
+/**
+ *
+ * @author RM-PC
+ */
+public class principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
